e39a997423b87db35bab49866659b4916716c28d
emanuelssj <emanuelolz@yahoo.com.br>
2019-06-19 14:40:08 -0300

    Added django heroku

--- challenge/settings.py
+++ challenge/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'y$@5=#l$bd2em=s2a6!-e)ljlbts7d*73mg7o5eicgde0ir^ba'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [['atmablogapp.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['atmablogapp.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
