6d416ddcfb98e1b56f822f3ec6c8587cb7f4fb6c
emanuelssj <emanuelolz@yahoo.com.br>
2019-06-19 16:50:44 -0300

    ...

--- challenge/settings.py
+++ challenge/settings.py
@@ -89,9 +89,9 @@ DATABASES = {
     }
 }
 #db_from_env = dj_database_url.config()
-#DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 #DATABASES['default'].update(db_from_env)
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
