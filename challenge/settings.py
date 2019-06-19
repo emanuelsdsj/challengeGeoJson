99a23aa499593c41aeb1aa073825f509c99192da
emanuelssj <emanuelolz@yahoo.com.br>
2019-06-19 17:03:15 -0300

    ...

--- challenge/settings.py
+++ challenge/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
 #db_from_env = dj_database_url.config()
 #DATABASES['default'].update(db_from_env)
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.spatialite'
 DATABASES['default']['NAME'] = 'de0otnc1l9s175'
 DATABASES['default']['USER'] = 'gakydrdrziabkt'
 DATABASES['default']['PASSWORD'] = 'c22c001b40521f7a0de1da21c9fc2b0891af130d0bc47fee69c6d036fd3b6acf'
