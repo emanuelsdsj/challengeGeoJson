e7109f0dbf886e0c2788ea29d7fb2ef6b25998d1
emanuelssj <emanuelolz@yahoo.com.br>
2019-06-19 14:00:01 -0300

    ...

--- challenge/settings.py
+++ challenge/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
